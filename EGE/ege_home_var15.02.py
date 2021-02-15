38d3da6087e5d32a5cab01e91aa1bb1df3669006
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-02-15 07:33:22 +0500

    product

--- EGE/ege_home_var15.02.py
+++ EGE/ege_home_var15.02.py
@@ -1,5 +1,4 @@
 from itertools import product
-print('x', 'y', 'z', 'w')
 for x, y, z, w in product(range(2), range(2), range(2), range(2)):
-    if (not(x == (not y)) or (y and (not z))) or (z and (not w)) is False:
-        print(x, y, z, w)
+    if (not x != y) or (y and (not z)) or (z and (not w)) is False:
+        print(w, z, y, x)
