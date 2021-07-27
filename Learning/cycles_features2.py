1183195ab82904fe728cc2a7753744170742ab25
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2021-07-27 12:24:12 +0500

    Really ALL combinations giving sum = 12

--- Learning/cycles_features2.py
+++ Learning/cycles_features2.py
@@ -9,18 +9,18 @@ def find_twelve(num_list1, num_list2, num_list3):
         for num2 in num_list2:
             for num3 in num_list3:
                 if num1 + num2 + num3 == 12:
-                    return num1, num2, num3
+                    print(num1, num2, num3)
 
 
 def find_twelve2(num_list1, num_list2, num_list3):
     for n1, n2, n3 in product(num_list1, num_list2, num_list3):
         if n1 + n2 + n3 == 12:
-            return n1, n2, n3
+            print(n1, n2, n3)
 
 
 num_list1 = [2, 3, 4, 3]
 num_list2 = [5, 5, 10, 2]
-num_list3 = [6, 4, 11, 3]
+num_list3 = [7, 4, 11, 7]
 
 
 print(find_twelve(num_list1, num_list2, num_list3))
